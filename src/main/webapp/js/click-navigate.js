24bb0f5e32608edfc94dc36a91977ffb1470aa38
AtanuPanja <atanupanja9463@gmail.com>
2024-08-17 12:51:12 +0530

    Update UI
    noteContent changed in index.jsp, is
    now updated in click-navigate.js.
    
    Removed previous comments in
    click-navigate.js
    
    Changed styles to allow pointer cursor
    only on noteContent, and not in
    noteFooter

--- src/main/webapp/js/click-navigate.js
+++ src/main/webapp/js/click-navigate.js
@@ -1,5 +1,5 @@
 const addNewNote = document.getElementById('add-new-note');
-const notes = document.getElementsByClassName('note-content');
+const notes = document.getElementsByClassName('noteContent');
 
 const contextPath = addNewNote.getAttribute('data-context-path');
 
@@ -11,18 +11,11 @@ Array.from(notes).forEach(element => {
 	
 	element.addEventListener('click', (event)=> {
 		const target = event.target;
-		// target.closest returns the element closest to the target
-		// i.e., closest to the element which is clicked upon
-		// It was needed because nested h3 and p element when clicked,
-		// had the id as null. I can't set the same id as div.card
-		// on its nested h3 and p elements.
-		// I had an option to set data-id on all three elements, which
-		// allows duplication. But with the closest method approach, it is
-		// not needed.
+		// get the id from the closest element with .card (closest will 
+		// be the parent)
 		const card = target.closest('.card');
 		if (card) {
 			const id = card.getAttribute('id');
-			console.log('clicked on note#'+id);
 			window.location.href = contextPath + '/edit-note/'+ id;
 		}
 		
